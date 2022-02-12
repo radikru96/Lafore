d7787c16b1cf0fa3ce6e0f3cbfaa21d25ea971ba
Radikru96 <radikru96@gmail.com>
2022-02-12 06:22:46 +0300

    tollBooth release

--- tollBooth/src/main.cpp
+++ tollBooth/src/main.cpp
@@ -1,39 +1,41 @@
 #include "tollBooth.h"
 #include <iostream>
-#include <ncurses.h>
+#include <termios.h>
+#include <unistd.h>
+#include <csignal>
 using namespace std;
 
-int Getch();
+tollBooth tb;
+struct termios old_tio, new_tio;
+
+void signalHandler( int signum )
+{
+	tcsetattr( STDIN_FILENO, TCSANOW, &old_tio );
+    cout << "End program. Car count = " << tb << "\nInterrupt signal (" << signum << ") received.\n";
+    exit( 0 );
+}
 
 int main()
 {
-	tollBooth tb;
-	int key;
+    std::signal( SIGINT, signalHandler );
+	unsigned char c;
 	bool noend = 1;
-	cout << "press:\t\"p\" for paying\n\t\"d\" for nopay\n\t\"Esc\" for print result and exit\n";
+	cout << "press:\t\"p\" for paying\n\t\"d\" for nopay\n\t\"Esc\" or Ctrl+C for print result and exit\n";
+	tcgetattr( STDIN_FILENO, &old_tio );
+	new_tio=old_tio;
+	new_tio.c_lflag &=(~ICANON & ~ECHO);
+	tcsetattr( STDIN_FILENO, TCSANOW, &new_tio );
 	while ( noend )
 	{
-		switch ( ( key = Getch() ) )
+		switch ( ( c = getchar() ) )
 		{
 		case 'p': tb.payingCar(); cout << "pressed p\n"; break;
-		case 'd': tb.nopayCar(); cout << "pressed p\n"; break;
+		case 'd': tb.nopayCar(); cout << "pressed d\n"; break;
 		case 27: cout << "pressed Esc\n"; noend=0; break;
-		default:
-			  cout << key << endl << tb << endl;
-			       break;
+		default: break;
 		}
 	}
 	cout << "End program. Car count = " << tb << endl;
+	tcsetattr( STDIN_FILENO, TCSANOW, &old_tio );
 	return 0;
-}
-
-int Getch()
-{
-	int key;
-	initscr();
-	endwin();
-	noecho();
-	keypad(stdscr, TRUE);
-	key = getch();
-	return key;
 }
